c18f3fae2a98d87ccfccfbb650db90fbe4f54086
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-09-25 21:20:33 +0700

    Complete UT03

--- 08_SiNguyenMinhTuan/UT04_Nguyen.cs
+++ 08_SiNguyenMinhTuan/UT04_Nguyen.cs
@@ -7,19 +7,9 @@ namespace _08_SiNguyenMinhTuan
     [TestClass]
     public class UT04_Nguyen
     {
-        public TestContext TestContext { get; set; }
-
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            "|DataDirectory|\\DataUT03.csv", "DataUT03#csv",
-           DataAccessMethod.Sequential), DeploymentItem("DataUT03.csv"), TestMethod]
-        public void TestMethod01()
+        [TestMethod]
+        public void TestMethod1()
         {
-            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
-            int tienDienCu = Convert.ToInt32(TestContext.DataRow[0]);
-            int tienDienMoi = Convert.ToInt32(TestContext.DataRow[1]);
-            double expectedResult = Convert.ToDouble(TestContext.DataRow[2]);
-            double actualResult = o.TinhTienDien(tienDienCu, tienDienMoi);
-            Assert.AreEqual(expectedResult, actualResult, 0.001); ;
         }
     }
 }
