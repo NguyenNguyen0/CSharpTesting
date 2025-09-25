582593c0ec8cbdd381bc343f815465476c4615e2
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-09-25 23:15:36 +0700

    Complete UT05

---
+++ 08_SiNguyenMinhTuan/UT05_Minh_DataDriven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _08_SiNguyenMinhTuan
+{
+    [TestClass]
+    public class UT05_Minh_DataDriven
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\DataUT05.csv", "DataUT05#csv",
+           DataAccessMethod.Sequential), DeploymentItem("DataUT05.csv"), TestMethod]
+        public void TestMethod01()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            string inputString = TestContext.DataRow["s"].ToString();
+            int n = Convert.ToInt32(TestContext.DataRow["n"]);
+            int p = Convert.ToInt32(TestContext.DataRow["p"]);
+            string expectedOutput = TestContext.DataRow["output"].ToString();
+
+            string actualOutput = o.HuyChuoi(inputString, n, p);
+
+            Assert.AreEqual(expectedOutput, actualOutput,
+                $"HuyChuoi failed with s='{inputString}', n={n}, p={p}. Expected: '{expectedOutput}', Actual: '{actualOutput}'");
+        }
+    }
+}
--- 08_SiNguyenMinhTuan/UT06_Minh.cs
+++ 08_SiNguyenMinhTuan/UT06_Minh.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace _08_SiNguyenMinhTuan
 {
-    internal class UT06_Nguyen
+    [TestClass]
+    public class UT06_Minh_DataDriven
     {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\DataUT03.csv", "DataUT03#csv",
+           DataAccessMethod.Sequential), DeploymentItem("DataUT03.csv"), TestMethod]
+        public void TestMethod01()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            int tienDienCu = Convert.ToInt32(TestContext.DataRow[0]);
+            int tienDienMoi = Convert.ToInt32(TestContext.DataRow[1]);
+            double expectedResult = Convert.ToDouble(TestContext.DataRow[2]);
+            double actualResult = o.TinhTienDien(tienDienCu, tienDienMoi);
+            Assert.AreEqual(expectedResult, actualResult, 0.001);
+        }
     }
 }
