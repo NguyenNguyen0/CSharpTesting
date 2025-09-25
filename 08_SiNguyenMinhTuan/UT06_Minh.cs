4ef60114b4feaf7b0ccac4aabc6851d607e85b24
Nguyen Van Minh <121565657+nvminh162@users.noreply.github.com>
2025-09-25 23:40:52 +0700

    UT06

--- 08_SiNguyenMinhTuan/UT06_Minh.cs
+++ 08_SiNguyenMinhTuan/UT06_Minh.cs
@@ -1,28 +1,72 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _08_SiNguyenMinhTuan
 {
     [TestClass]
-    public class UT06_Minh_DataDriven
+    public class UT06_Minh
     {
-        public TestContext TestContext { get; set; }
-
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            "|DataDirectory|\\DataUT03.csv", "DataUT03#csv",
-           DataAccessMethod.Sequential), DeploymentItem("DataUT03.csv"), TestMethod]
-        public void TestMethod01()
+        [TestMethod]
+        public void TestMethod1()
         {
             MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
-            int tienDienCu = Convert.ToInt32(TestContext.DataRow[0]);
-            int tienDienMoi = Convert.ToInt32(TestContext.DataRow[1]);
-            double expectedResult = Convert.ToDouble(TestContext.DataRow[2]);
-            double actualResult = o.TinhTienDien(tienDienCu, tienDienMoi);
-            Assert.AreEqual(expectedResult, actualResult, 0.001);
+            String actual = o.ThayThe("", "", "");
+            String expected = "";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abcdef", "cd", "ss");
+            String expected = "abssef";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abcdef", "", "ss");
+            String expected = "abssef";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abcdef", "ss", "gh");
+            String expected = "abcdef";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abcdef", "cd", "");
+            String expected = "abef";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abc", "ab", "X");
+            String expected = "Xc";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            String actual = o.ThayThe("abc", "cd", "D");
+            String expected = "abD";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
