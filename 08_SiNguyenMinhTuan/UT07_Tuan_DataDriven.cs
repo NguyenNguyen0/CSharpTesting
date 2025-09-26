daab72ace23570d8481001ee4a6b75a8f53d74ca
tuan <daoquoctuan972@gmail.com>
2025-09-26 22:22:35 +0700

    complete ut lab 7, 8

--- 08_SiNguyenMinhTuan/UT07_Tuan_DataDriven.cs
+++ 08_SiNguyenMinhTuan/UT07_Tuan_DataDriven.cs
@@ -10,15 +10,17 @@ namespace _08_SiNguyenMinhTuan
         public TestContext TestContext { get; set; }
 
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-            "|DataDirectory|\\DataUT07.csv", "DataUT07#csv",
-           DataAccessMethod.Sequential), DeploymentItem("DataUT07.csv"), TestMethod]
+            "|DataDirectory|\\DataUT07.csv", "DataUT07#csv", DataAccessMethod.Sequential),
+            DeploymentItem("DataUT07.csv"), TestMethod()]
         public void TestMethod1()
         {
             MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
-            string arrayStr = TestContext.DataRow["array"].ToString();
+            string arrayStr = TestContext.DataRow[0].ToString();
             arrayStr = arrayStr.Trim('{', '}');
-            int[] numbers = arrayStr.Split(';').Select(int.Parse).ToArray();
-            int expectedMax = Convert.ToInt32(TestContext.DataRow["max"]);
+            int[] numbers = arrayStr.Length == 0
+                ? Array.Empty<int>()
+                : arrayStr.Split(';').Select(int.Parse).ToArray();
+            int expectedMax = Convert.ToInt32(TestContext.DataRow[1]);
             int actualMax = o.Largest(numbers);
             Console.WriteLine($"Array: {{{string.Join(", ", numbers)}}}, Expected Max: {expectedMax}, Actual Max: {actualMax}");
             Assert.AreEqual(expectedMax, actualMax);
--- 08_SiNguyenMinhTuan/UT08_Tuan.cs
+++ 08_SiNguyenMinhTuan/UT08_Tuan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace _08_SiNguyenMinhTuan
 {
-    internal class UT08_Tuan
+    [TestClass]
+    public class UT08_Tuan
     {
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\DataUT08.csv", "DataUT08#csv", DataAccessMethod.Sequential),
+            DeploymentItem("DataUT08.csv"), TestMethod()]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            string arrayStr = TestContext.DataRow[0].ToString();
+            arrayStr = arrayStr.Trim('{', '}');
+            int[] list = arrayStr.Length == 0
+                ? Array.Empty<int>()
+                : arrayStr.Split(';').Select(int.Parse).ToArray();
+
+            int left = Convert.ToInt32(TestContext.DataRow[1]);
+            int right = Convert.ToInt32(TestContext.DataRow[2]);
+
+            string expected = TestContext.DataRow[3].ToString();
+            expected = expected.Trim('{', '}');
+            int[] expectedl = expected.Length == 0
+                ? Array.Empty<int>()
+                : expected.Split(';').Select(int.Parse).ToArray();
+
+            o.QuickSort(list, left, right);
+
+            string actualStr = string.Join(";", list);
+            string expectedOut = string.Join(";", expectedl);
+
+            Console.WriteLine($"Sorted: {actualStr}, Expected: {expectedOut}");
+            Assert.AreEqual(expectedOut, actualStr);
+        }
     }
 }
